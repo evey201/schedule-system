c90c2a7cf771c1ee95cb8f308ffd40702ebaa074
Eveshonyanime Ocheche-Alabi <evesho.alabi@gmail.com>
2023-07-18 22:58:00 +0300

    chore:added oute protection to sidebar

--- src/components/Layouts/dashboard/sidebar/Sidebar.tsx
+++ src/components/Layouts/dashboard/sidebar/Sidebar.tsx
@@ -1,8 +1,15 @@
 import { useAuth } from '../../../../hooks'
 import { SidebarContainer } from './Sidebar.styled'
 
+interface UserData {
+    role: string
+}
+
 export const Sidebar = () => {
-    const { logout } = useAuth()
+    const { logout, state } = useAuth()
+    const userData = state.data as UserData;
+    const role = userData?.role;
+    
     return (
         <>
             <SidebarContainer className="flex h-screen w-16 flex-col justify-between border-e bg-white">
@@ -34,64 +41,66 @@ export const Sidebar = () => {
                         </span>
                     </a>
                     </div>
+                    {
+                        role === 'manager' && (
+                            <ul className="space-y-1 border-t border-gray-100 pt-4">
+                                <li>
+                                    <a
+                                    href="/teams"
+                                    className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                                    >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 opacity-75"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                    >
+                                        <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                                        />
+                                    </svg>
 
-                    <ul className="space-y-1 border-t border-gray-100 pt-4">
-                    <li>
-                        <a
-                        href="/teams"
-                        className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                        >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 opacity-75"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-                            />
-                        </svg>
+                                    <span
+                                        className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
+                                    >
+                                        Teams
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                    href="/game-tables"
+                                    className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                                    >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 opacity-75"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                    >
+                                        <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                                        />
+                                    </svg>
 
-                        <span
-                            className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
-                        >
-                            Teams
-                        </span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                        href="/game-tables"
-                        className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                        >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 opacity-75"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                            />
-                        </svg>
-
-                        <span
-                            className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
-                        >
-                            Tables
-                        </span>
-                        </a>
-                    </li>
-                    </ul>
+                                    <span
+                                        className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
+                                    >
+                                        Tables
+                                    </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        )
+                    }
                 </div>
                 </div>
             </div>
--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../hooks";
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { Loading } from "../components";
 
 type RoleType = {
     role?: string[]
 }
 
 export const ProtectedRoute = ({ role, ...props }: RoleType) => {
-    console.log('props', role)
     const { state } = useAuth();
-
-    return state.authenticated ? <Outlet {...props} /> : <Navigate to="/login" />
+    const [loading, setLoading] = useState<boolean>(true)
+    const navigate = useNavigate()
+    useEffect(() => {
+        setLoading(true)
+        if (state.authenticated === false) {
+            navigate('/login')
+        }
+        setLoading(false)
+    }, [navigate, role, state.authenticated])
+    // if (!role?.includes('manager')) return loading ? <Loading />: <Navigate to="/" />
+    return loading ? <Loading />: <Outlet {...props} />
 }
